912bb340210469d0593a513d42aa2187d143591f
laisdejesus <dejesusll@outlook.com>
2022-01-11 21:16:24 -0300

    Band name Generator Program

---
+++ day001/band_name_generator.py
@@ -0,0 +1,5 @@
+city = str(input("What's your hometown? "))
+pet = str(input("What's you pet name in childhood? "))
+
+print("Generating name for your band ...")
+print("The name for your band is {} {}".format(city, pet))
